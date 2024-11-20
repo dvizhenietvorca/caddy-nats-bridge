2e5d91832371328232c212e36d3962b7ef5d6eec
dvizhenietvorca <35180583+dvizhenietvorca@users.noreply.github.com>
2024-11-21 01:58:02 +0300

    Update http_msg_to_nats_msg.go

--- common/http_msg_to_nats_msg.go
+++ common/http_msg_to_nats_msg.go
@@ -30,7 +30,7 @@ func NatsMsgForHttpRequest(r *http.Request, subject string) (*nats.Msg, error) {
 
 	msg.Header.Add("X-NatsBridge-Host", r.Host) //DT
 	msg.Header.Add("X-NatsBridge-RemoteAddr", r.RemoteAddr) //DT
-	msg.Header.Add("X-NatsBridge-Header", r.Header..String()) //DT
+	msg.Header.Add("X-NatsBridge-Header", r.Header.String()) //DT
 	
 	return msg, nil
 }
